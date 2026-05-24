db66a35affa6ade1275dab45a077690097eba384
chrshh <chrisholsonback358@gmail.com>
2026-05-23 23:18:08 -0500

    [TERM] basic esc codes handled

--- userland/apps/squishy/src/parser.rs
+++ userland/apps/squishy/src/parser.rs
@@ -16,6 +16,15 @@ impl<'a> vte::Perform for Performer<'a> {
         self.grid.put(self.cursor.col, self.cursor.row, c);
         self.cursor.col += 1;
     }
+
+    fn execute(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.cursor.row += 1,
+            b'\r' => self.cursor.col = 0,
+            b'\x08' => self.cursor.col = self.cursor.col.saturating_sub(1),
+            _ => (),
+        }
+    }
 }
 
 pub fn feed(parser: &mut vte::Parser, grid: &mut Grid, cursor: &mut Cursor, bytes: &[u8]) {
@@ -25,14 +34,38 @@ pub fn feed(parser: &mut vte::Parser, grid: &mut Grid, cursor: &mut Cursor, byte
     parser.advance(&mut performer, bytes);
 }
 
-#[test]
-fn prints_hello_world() {
-    let mut grid = Grid::new(80, 24);
-    let mut cursor = Cursor { col: 0, row: 0 };
-    let mut parser = vte::Parser::new();
-    feed(&mut parser, &mut grid, &mut cursor, b"hello");
-    assert_eq!(grid.cells[0].ch, 'h');
-    assert_eq!(grid.cells[1].ch, 'e');
-    assert_eq!(grid.cells[2].ch, 'l');
-    assert_eq!(grid.cells[3].ch, 'l');
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn prints_hello_world() {
+        let mut grid = Grid::new(80, 24);
+        let mut cursor = Cursor { col: 0, row: 0 };
+        let mut parser = vte::Parser::new();
+        feed(&mut parser, &mut grid, &mut cursor, b"hello");
+        assert_eq!(grid.cells[0].ch, 'h');
+        assert_eq!(grid.cells[1].ch, 'e');
+        assert_eq!(grid.cells[2].ch, 'l');
+        assert_eq!(grid.cells[3].ch, 'l');
+    }
+
+    #[test]
+    fn backspace() {
+        let mut grid = Grid::new(80, 24);
+        let mut cursor = Cursor { col: 0, row: 0 };
+        let mut parser = vte::Parser::new();
+        feed(&mut parser, &mut grid, &mut cursor, b"hello\x08");
+        assert_eq!(cursor.col, 4);
+    }
+
+    #[test]
+    fn newline() {
+        let mut grid = Grid::new(80, 24);
+        let mut cursor = Cursor { col: 0, row: 0 };
+        let mut parser = vte::Parser::new();
+        feed(&mut parser, &mut grid, &mut cursor, b"hello\r\n");
+        assert_eq!(cursor.row, 1);
+        assert_eq!(cursor.col, 0);
+    }
 }
